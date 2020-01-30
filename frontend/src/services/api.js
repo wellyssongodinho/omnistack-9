37e24c457d4db778c1e734f944da6c285e8a09d7
Wellysson Godinho <wellysson.gomes@gmail.com>
2020-01-30 17:21:36 -0300

    Update Procfile for Heroku and .env

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: ' https://aircnc-rocketseat.herokuapp.com'//'http://localhost:3333',
+  baseURL: 'https://aircnc-rocketseat.herokuapp.com/'//'http://localhost:3333',
 });
 
 export default api;
